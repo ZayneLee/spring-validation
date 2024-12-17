bda7a9b1a289692c0fb59cdaeea225e4aeae429e
star1582@naver.com <star1582@naver.com>
2024-12-17 21:53:50 +0900

    검증 로직 개발

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -3,13 +3,19 @@ package hello.itemservice.web.validation;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @Controller
 @RequestMapping("/validation/v1/items")
 @RequiredArgsConstructor
@@ -38,7 +44,35 @@ public class ValidationItemControllerV1 {
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        if (!StringUtils.hasText(item.getItemName())) {
+            errors.put("itemName", "상품 이름은 필수 입니다");
+        }
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            errors.put("price", "상품 가격은 1,000 ~ 1,000,000 원 입니다.");
+        }
+
+        if (item.getQuantity() == null || item.getQuantity() > 9999) {
+            errors.put("quantity", "상품 수량은 9999 개 이하 입니다.");
+        }
+
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int result = item.getPrice() * item.getQuantity();
+            if (result < 10000) {
+                errors.put("erros", "상품 가격 * 수량은 10,000 원 이상이어야 합니다. 현재 가격은 " + result);
+            }
+        }
+
+        if (!errors.isEmpty()) {
+            log.info("errors = {}", errors);
+            model.addAttribute("errors", errors);
+            return "validation/v1/addForm";
+        }
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -59,4 +93,3 @@ public class ValidationItemControllerV1 {
     }
 
 }
-
