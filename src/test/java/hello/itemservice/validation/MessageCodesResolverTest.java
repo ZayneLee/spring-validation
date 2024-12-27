64fe9733190131e1a135f6b3258ef82971f6ac43
zayne-sqtech <sdlee@ezfarm.co.kr>
2024-12-27 17:17:15 +0900

    테스트 케이스 작성 #9

--- src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
+++ src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -1,6 +1,7 @@
 package hello.itemservice.validation;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.validation.DefaultMessageCodesResolver;
 import org.springframework.validation.MessageCodesResolver;
@@ -12,6 +13,13 @@ public class MessageCodesResolverTest {
     @Test
     void messageCodesResolverObject() {
         String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
-        Assertions.assertThat(messageCodes).containsExactly("required.item", "required");
+        assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageCodesResolverField() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        assertThat(messageCodes).containsExactly("required.item.itemName", "required.itemName",
+                "required.java.lang.String", "required");
     }
 }
