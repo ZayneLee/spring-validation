e8593e90e5591a3a74b77f263c599af0bdd26def
ZayneLee <star1582@naver.com>
2024-12-30 10:14:04 +0900

    Git Test 2

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -201,6 +201,7 @@ public class ValidationItemControllerV2 {
     }
 
     // Git Test Comment
+    // Git Test Comment2
     @PostMapping("/add")
     public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult,
             RedirectAttributes redirectAttributes) {
